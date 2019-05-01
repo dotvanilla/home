f463547a73e4f058b8bcf39c114cd9489db2ca0d
xieguigang <xie.guigang@gcmodeller.org>
2019-05-01 17:47:26 +0800

    update script

--- javascripts/webgl.render.d.ts
+++ javascripts/webgl.render.d.ts
@@ -1,3 +1,48 @@
-declare module "app" {
-    export default function initRender(circleFloat32Array: any, circleCount: any, init?: () => void, timeStep?: () => void): void;
+declare namespace webgl {
+    interface CanvasFrameAction {
+        (width: number, height: number): void;
+    }
+    /**
+     * A typescript rewrite of https://github.com/guybedford/wasm-demo/blob/master/lib/render.js
+    */
+    class render {
+        private canvas;
+        private timeStep;
+        private init;
+        private circleFloat32Array;
+        private circleCount;
+        gl: WebGLRenderingContext;
+        /**
+         * Program and shaders
+        */
+        program: WebGLProgram;
+        /**
+         * Viewport
+        */
+        u_resolutionLoc: WebGLUniformLocation;
+        displayWidth: number;
+        displayHeight: number;
+        iterations: number;
+        lastDrawTime: number;
+        iterationConvergence: number;
+        convergedIterations: number;
+        /**
+         * initRender
+         *
+         * @param init call generation function
+        */
+        constructor(canvas: HTMLCanvasElement, timeStep: CanvasFrameAction, init: CanvasFrameAction, circleFloat32Array: ArrayBuffer, circleCount: number);
+        /**
+         * Draw loop
+        */
+        private draw;
+        private updateIterationCount;
+        private static initDevice;
+        resize(): void;
+    }
+}
+declare namespace webgl.shaders {
+    const VERTEX_SHADER: string;
+    const FRAGMENT_SHADER: string;
+    function compileShader(gl: WebGLRenderingContext, shaderType: number, shaderSource: string): WebGLShader;
 }
--- javascripts/webgl.render.js
+++ javascripts/webgl.render.js
@@ -1,21 +1,120 @@
-define("app", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function initRender(circleFloat32Array, circleCount, init = () => { }, timeStep = () => { }) {
-        const gl = canvas.getContext('webgl2');
-        /*
-         * Program and shaders
-         */
-        const program = gl.createProgram();
-        function compileShader(gl, shaderType, shaderSource) {
-            const shader = gl.createShader(shaderType);
-            gl.shaderSource(shader, shaderSource);
-            gl.compileShader(shader);
-            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-                throw new Error('Could not compile shader: ' + gl.getShaderInfoLog(shader));
-            return shader;
+var webgl;
+(function (webgl) {
+    /*
+     * Naive iteration count optimization
+     */
+    const maxIterations = 64;
+    /**
+     * A typescript rewrite of https://github.com/guybedford/wasm-demo/blob/master/lib/render.js
+    */
+    class render {
+        /**
+         * initRender
+         *
+         * @param init call generation function
+        */
+        constructor(canvas, timeStep, init, circleFloat32Array, circleCount) {
+            this.canvas = canvas;
+            this.timeStep = timeStep;
+            this.init = init;
+            this.circleFloat32Array = circleFloat32Array;
+            this.circleCount = circleCount;
+            this.iterations = 1;
+            this.iterationConvergence = 0;
+            this.convergedIterations = 0;
+            var vm = this;
+            this.gl = canvas.getContext('webgl2');
+            this.program = this.gl.createProgram();
+            render.initDevice(this.gl, this.program);
+            this.u_resolutionLoc = this.gl.getUniformLocation(this.program, 'u_resolution');
+            this.resize();
+            window.addEventListener('resize', vm.resize, false);
+            init(this.displayWidth, this.displayHeight);
+            /**
+             * Bind circle data buffer
+            */
+            const circleCoordLoc = this.gl.getAttribLocation(this.program, 'circleCoord');
+            this.gl.enableVertexAttribArray(circleCoordLoc);
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.gl.createBuffer());
+            this.gl.vertexAttribPointer(circleCoordLoc, 3, this.gl.FLOAT, false, 4 * 3, 0);
+            this.lastDrawTime = performance.now();
+            requestAnimationFrame(vm.draw);
         }
-        gl.attachShader(program, compileShader(gl, gl.VERTEX_SHADER, `#version 300 es
+        /**
+         * Draw loop
+        */
+        draw() {
+            let curTime = performance.now();
+            let gl = this.gl;
+            // optimize iteration count from time since last draw
+            this.updateIterationCount(curTime - this.lastDrawTime);
+            for (let i = 0; i < this.iterations; i++) {
+                this.timeStep(this.displayWidth, this.displayHeight);
+            }
+            // copy the circle data from array buffer memory to the GPU
+            gl.bufferData(gl.ARRAY_BUFFER, this.circleFloat32Array, gl.DYNAMIC_DRAW);
+            // draw the circles on the GPU from the data
+            gl.drawArrays(gl.POINTS, 0, this.circleCount);
+            // start a new animation loop
+            this.lastDrawTime = curTime;
+            requestAnimationFrame(this.draw);
+        }
+        updateIterationCount(dt) {
+            if (dt < 17 && this.iterations < maxIterations / 2) {
+                this.iterationConvergence = 0;
+                this.iterations *= 2;
+            }
+            else if (dt < 20 && this.iterations < maxIterations) {
+                this.iterationConvergence = 0;
+                this.iterations++;
+            }
+            else if (dt > 55 && this.iterations > 1) {
+                this.iterationConvergence = 0;
+                this.iterations--;
+            }
+            if (this.iterationConvergence < 10) {
+                this.iterationConvergence++;
+                if (this.iterationConvergence === 10 && this.convergedIterations !== this.iterations) {
+                    this.convergedIterations = this.iterations;
+                    console.log(`timeStep of ${this.iterations} iterations/frame, ~${Math.round(1000 / dt)} frames/sec.`);
+                }
+            }
+        }
+        static initDevice(gl, program) {
+            gl.attachShader(program, webgl.shaders.compileShader(gl, gl.VERTEX_SHADER, webgl.shaders.VERTEX_SHADER));
+            gl.attachShader(program, webgl.shaders.compileShader(gl, gl.FRAGMENT_SHADER, webgl.shaders.FRAGMENT_SHADER));
+            gl.linkProgram(program);
+            if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+                throw `Unable to link program:  ${gl.getProgramInfoLog(program)}`;
+                ;
+            }
+            gl.useProgram(program);
+            /**
+             * Blending
+            */
+            gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+            gl.enable(gl.BLEND);
+        }
+        resize() {
+            let canvas = this.gl.canvas;
+            this.displayWidth = canvas.clientWidth;
+            this.displayHeight = canvas.clientHeight;
+            if (canvas.width !== this.displayWidth || canvas.height !== this.displayHeight) {
+                canvas.width = this.displayWidth;
+                canvas.height = this.displayHeight;
+                this.gl.viewport(0, 0, this.displayWidth, this.displayHeight);
+                // update the resolution global uniform on the GPU
+                this.gl.uniform2f(this.u_resolutionLoc, canvas.width, canvas.height);
+            }
+        }
+    }
+    webgl.render = render;
+})(webgl || (webgl = {}));
+var webgl;
+(function (webgl) {
+    var shaders;
+    (function (shaders) {
+        shaders.VERTEX_SHADER = `#version 300 es
     in vec3 circleCoord;
     uniform vec2 u_resolution;
     out float size;
@@ -26,9 +125,8 @@ define("app", ["require", "exports"], function (require, exports) {
       gl_Position = vec4(circle, 0, 1);
       // set circle size from z coord of circleCoord (gl.POINT render option)
       gl_PointSize = size = circleCoord.z * 2.0 + 1.0;
-    }
-  `));
-        gl.attachShader(program, compileShader(gl, gl.FRAGMENT_SHADER, `#version 300 es
+    }`;
+        shaders.FRAGMENT_SHADER = `#version 300 es
     precision mediump float;
     in float size;
     out vec4 outColor;
@@ -46,94 +144,17 @@ define("app", ["require", "exports"], function (require, exports) {
       outColor = vec4(((size - 2.5) / 100.0), 0.2, 0.3, alpha);
       // pre-multiply alpha... something about blending edges
       outColor.rgb *= outColor.a;
-    }
-  `));
-        gl.linkProgram(program);
-        if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-            throw new Error('Unable to link program: ' + gl.getProgramInfoLog(program));
-        gl.useProgram(program);
-        /*
-         * Blending
-         */
-        gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-        gl.enable(gl.BLEND);
-        /*
-         * Viewport
-         */
-        const u_resolutionLoc = gl.getUniformLocation(program, 'u_resolution');
-        let displayWidth, displayHeight;
-        function resize() {
-            const canvas = gl.canvas;
-            displayWidth = canvas.clientWidth;
-            displayHeight = canvas.clientHeight;
-            if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
-                canvas.width = displayWidth;
-                canvas.height = displayHeight;
-                gl.viewport(0, 0, displayWidth, displayHeight);
-                // update the resolution global uniform on the GPU
-                gl.uniform2f(u_resolutionLoc, canvas.width, canvas.height);
+    }`;
+        function compileShader(gl, shaderType, shaderSource) {
+            const shader = gl.createShader(shaderType);
+            gl.shaderSource(shader, shaderSource);
+            gl.compileShader(shader);
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+                throw `Could not compile shader:  ${gl.getShaderInfoLog(shader)}`;
             }
+            return shader;
         }
-        resize();
-        window.addEventListener('resize', resize, false);
-        // call generation function
-        init(displayWidth, displayHeight);
-        /*
-         * Bind circle data buffer
-         */
-        const circleCoordLoc = gl.getAttribLocation(program, 'circleCoord');
-        gl.enableVertexAttribArray(circleCoordLoc);
-        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-        gl.vertexAttribPointer(circleCoordLoc, 3, gl.FLOAT, false, 4 * 3, 0);
-        /*
-         * Draw loop
-         */
-        let iterations = 1;
-        let lastDrawTime = performance.now();
-        requestAnimationFrame(draw);
-        function draw() {
-            let curTime = performance.now();
-            // optimize iteration count from time since last draw
-            updateIterationCount(curTime - lastDrawTime);
-            for (let i = 0; i < iterations; i++)
-                timeStep(displayWidth, displayHeight);
-            // copy the circle data from array buffer memory to the GPU
-            gl.bufferData(gl.ARRAY_BUFFER, circleFloat32Array, gl.DYNAMIC_DRAW);
-            // draw the circles on the GPU from the data
-            gl.drawArrays(gl.POINTS, 0, circleCount);
-            // animation loop
-            lastDrawTime = curTime;
-            requestAnimationFrame(draw);
-        }
-        /*
-         * Naive iteration count optimization
-         */
-        const maxIterations = 64;
-        let iterationConvergence = 0;
-        let convergedIterations = 0;
-        function updateIterationCount(dt) {
-            if (dt < 17 && iterations < maxIterations / 2) {
-                iterationConvergence = 0;
-                iterations *= 2;
-            }
-            else if (dt < 20 && iterations < maxIterations) {
-                iterationConvergence = 0;
-                iterations++;
-            }
-            else if (dt > 55 && iterations > 1) {
-                iterationConvergence = 0;
-                iterations--;
-            }
-            if (iterationConvergence < 10) {
-                iterationConvergence++;
-                if (iterationConvergence === 10 && convergedIterations !== iterations) {
-                    convergedIterations = iterations;
-                    console.log(`timeStep of ${iterations} iterations/frame, ~${Math.round(1000 / dt)} frames/sec.`);
-                }
-            }
-        }
-    }
-    exports.default = initRender;
-    ;
-});
+        shaders.compileShader = compileShader;
+    })(shaders = webgl.shaders || (webgl.shaders = {}));
+})(webgl || (webgl = {}));
 //# sourceMappingURL=webgl.render.js.map
