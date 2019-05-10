0c6df57862130c3cebc1cce2e84c73f1ad86e52c
xieguigang <xie.guigang@gcmodeller.org>
2019-05-10 17:13:29 +0800

    removes uncompress js files

---
+++ javascripts/compress.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+SET tool=closure-compiler-v20181125.jar
+
+java -jar %tool% --js_output_file=../linq.min.js ../linq.js
+java -jar %tool% --js_output_file=../marked.min.js ../marked.js
+java -jar %tool% --js_output_file=../vbcode.min.js ../vbcode.js
+java -jar %tool% --js_output_file=../visualbasic.wasm.min.js ../visualbasic.wasm.js
+java -jar %tool% --js_output_file=../webgl.render.min.js ../webgl.render.js
+
+pause
