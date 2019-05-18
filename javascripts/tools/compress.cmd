21f7d551a527c6091df61fa29bacbc80b4402b2e
xieguigang <xie.guigang@gcmodeller.org>
2019-05-18 22:58:11 +0800

    add wast highlights

--- javascripts/tools/compress.cmd
+++ javascripts/tools/compress.cmd
@@ -7,3 +7,4 @@ java -jar %tool% --js_output_file=../marked.min.js ../marked.js
 java -jar %tool% --js_output_file=../vbcode.min.js ../vbcode.js
 java -jar %tool% --js_output_file=../visualbasic.wasm.min.js ../visualbasic.wasm.js
 java -jar %tool% --js_output_file=../webgl.render.min.js ../webgl.render.js
+java -jar %tool% --js_output_file=../wast.code.min.js ../wast.code.js
